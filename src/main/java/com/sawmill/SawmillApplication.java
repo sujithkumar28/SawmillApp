b6e8fa0111867161a58c584e92b47afb70e78b1e
sujithkumar28 <sujithk28@gmail.com>
2022-02-16 16:09:40 +0530

    Update SawmillApplication.java

--- src/main/java/com/sawmill/SawmillApplication.java
+++ src/main/java/com/sawmill/SawmillApplication.java
@@ -2,8 +2,10 @@ package com.sawmill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class SawmillApplication {
 
 	public static void main(String[] args) {
